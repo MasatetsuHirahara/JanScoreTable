5165c0b64f1ec7629f5543c57c4d20bb1a400faf
MasatetsuHirahara <hirahara.masatetsu@gmail.com>
2022-06-05 00:02:58 +0900

    recentlyMember追加

---
+++ lib/src/accessor/recentyMember.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_app/src/common/const.dart';
+import 'package:flutter_app/src/model/memberModel.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'dbAccessor.dart';
+
+class RecentlyMemberAccessor extends ChangeNotifier {
+  RecentlyMemberAccessor(this.ref);
+  Ref ref;
+  List<MemberModel> memberList = [];
+
+  Future<void> get(int limit) async {
+    final dba = ref.read(dbAccessor);
+    if (dba.isOpen == false) {
+      return;
+    }
+
+    final sql = 'SELECT * FROM'
+        '$tableMember '
+        'ORDER BY $columnLastJoin DESC '
+        'LIMIT $limit';
+
+    final list = await dba.rawQuery(sql);
+
+    memberList = [];
+    for (final r in list) {
+      memberList.add(MemberModel.fromMap(r));
+    }
+
+    notifyListeners();
+  }
+}
