0efb859d16f1611b10fdb2c5d307cd208ed5bb92
MasatetsuHirahara <hirahara.masatetsu@gmail.com>
2022-06-05 13:04:42 +0900

    adjustment整理

--- lib/src/page/adjustment/view.dart
+++ lib/src/page/adjustment/view.dart
@@ -1,157 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_app/src/accessor/table/gameSettingProvider.dart';
-import 'package:flutter_app/src/model/gameSettingModel.dart';
+import 'package:flutter_app/src/page/adjustment/viewModel.dart';
 import 'package:flutter_app/src/widget/text.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-import '../../accessor/table/chipScoreProvider.dart';
-import '../../accessor/table/scoreProvider.dart';
 
 final _viewModel = ChangeNotifierProvider.autoDispose
     .family<AdjustmentViewModel, int>((ref, drId) {
   return AdjustmentViewModel(ref, drId);
 });
-
-class AdjustmentViewModel extends ChangeNotifier {
-  AdjustmentViewModel(this.ref, this.drId) {
-    listenGameSetting();
-    listenScore();
-    listenChipScore();
-  }
-
-  Ref ref;
-  int drId;
-  List<PointProperty> pointList = [];
-  GameSettingModel gameSetting;
-  List<int> totalList = [];
-  List<ChipScoreModelEx> chipScoreList;
-  TextEditingController placeFeeController = TextEditingController()
-    ..text = '0';
-
-  void setPointPropertyList() {
-    if (gameSetting == null) {
-      return;
-    }
-    if (chipScoreList == null) {
-      return;
-    }
-
-    // 場代をセット
-    placeFeeController
-      ..text = gameSetting.placeFee.toString()
-      ..selection = TextSelection.fromPosition(
-        TextPosition(offset: placeFeeController.text.length),
-      );
-
-    // 一人あたりの場代を計算。１の位は余りとしてトップに分配
-    var placeFeePerOne = 0;
-    var reminder = 0;
-    final placeFee = gameSetting.placeFee;
-    if (placeFee != 0) {
-      placeFeePerOne = (placeFee ~/ pointList.length);
-      final onesPlace = int.parse(placeFeePerOne.toString().substring(0, 1));
-      placeFeePerOne -= onesPlace;
-      reminder = (placeFee % pointList.length) + onesPlace * pointList.length;
-    }
-
-    // 表示するポイントを計算する
-    // chipScoreListはgjm分のindexを持っているのでchipScoreをベースにする
-    pointList = [];
-    var topIndex = 0;
-    var topScore = 0;
-    for (final cs in chipScoreList) {
-      final name = cs.name;
-      final chipPoint = cs.score * gameSetting.chipRate;
-      var point = chipPoint;
-      if (totalList.length > cs.number) {
-        final total = totalList[cs.number];
-        if (total > topScore) {
-          topIndex = cs.number;
-          topScore = total;
-        }
-        point += totalList[cs.number] * gameSetting.rate;
-      }
-      pointList.add(PointProperty(name, point, chipPoint, placeFeePerOne));
-    }
-
-    // topにあまりを追加
-    pointList[topIndex].addPlaceFee(reminder);
-
-    notifyListeners();
-  }
-
-  void listenGameSetting() {
-    final accessor = ref.watch(gameSettingAccessor);
-    if (accessor.isInitialized) {
-      if (accessor.drIdMap.containsKey(drId)) {
-        gameSetting = accessor.drIdMap[drId];
-        setPointPropertyList();
-      }
-    }
-  }
-
-  void listenScore() {
-    final accessor = ref.watch(scoreAccessor);
-    if (accessor.isInitialized) {
-      if (accessor.scoreViewMap.containsKey(drId)) {
-        final drIdScoreView = accessor.scoreViewMap[drId];
-        totalList = [];
-        for (var i = 0; i <= drIdScoreView.maxGameCount; i++) {
-          for (var j = 0; j <= drIdScoreView.maxNumber; j++) {
-            if (drIdScoreView.map[i].containsKey(j) == false) {
-              continue;
-            }
-            if (i == 0) {
-              totalList.add(drIdScoreView.map[i][j].score);
-              continue;
-            }
-            totalList[j] += drIdScoreView.map[i][j].score;
-          }
-        }
-        setPointPropertyList();
-      }
-    }
-  }
-
-  void listenChipScore() {
-    final accessor = ref.watch(chipScoreAccessor);
-    if (accessor.isInitialized) {
-      if (accessor.drIdMap.containsKey(drId)) {
-        chipScoreList = accessor.drIdMap[drId];
-        setPointPropertyList();
-      }
-    }
-  }
-
-  void afterPlaceFeeInput() {
-    final fee = int.parse(placeFeeController.text);
-    if (fee == null) {
-      return;
-    }
-    gameSetting.placeFee = fee;
-    ref.read(gameSettingAccessor).upsert(gameSetting);
-  }
-}
-
-class PointProperty {
-  PointProperty(this.name, this.point, this.chipPoint, this.placeFee) {}
-
-  String name;
-  int point;
-  int chipPoint;
-  int placeFee;
-
-  int get totalPoint {
-    return point + chipPoint - placeFee;
-  }
-
-  void addPlaceFee(int fee) {
-    placeFee += fee;
-  }
-}
 
 const cellHeight = 50.0;
 
@@ -165,11 +23,11 @@ class PlaceFeeCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 0,
-      shape: RoundedRectangleBorder(
+      shape: const RoundedRectangleBorder(
         side: BorderSide(
           color: Colors.black,
         ),
-        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
       ),
       child: ListTile(
         title: HeadingText('場代'),
@@ -199,11 +57,11 @@ class PointCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 0,
-      shape: RoundedRectangleBorder(
+      shape: const RoundedRectangleBorder(
         side: BorderSide(
           color: Colors.black,
         ),
-        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        borderRadius: BorderRadius.all(Radius.circular(12)),
       ),
       child: ExpansionTile(
         title: HeadingText(property.name),
@@ -217,7 +75,7 @@ class PointCard extends StatelessWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Text('スコア  '),
+                const Text('スコア  '),
                 ScoreText(
                   property.point,
                   trailing: 'G',
@@ -230,7 +88,7 @@ class PointCard extends StatelessWidget {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Text('チップ代  '),
+                const Text('チップ代  '),
                 ScoreText(
                   property.chipPoint,
                   trailing: 'G',
@@ -238,7 +96,7 @@ class PointCard extends StatelessWidget {
               ],
             ),
           ),
-          SizedBox(
+          const SizedBox(
             height: 8.0,
           )
         ],
@@ -274,20 +132,20 @@ class AdjustmentPage extends ConsumerWidget {
 
     return Scaffold(
         appBar: AppBar(
-          title: Text('精算'),
+          title: const Text('精算'),
           leading: IconButton(
-            icon: Icon(Icons.clear),
+            icon: const Icon(Icons.clear),
             onPressed: () => Navigator.of(context).pop(),
           ),
         ),
         body: SingleChildScrollView(
           child: SafeArea(
             child: Padding(
-              padding: EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
                   for (final p in provider.pointList) PointCard(p),
-                  SizedBox(
+                  const SizedBox(
                     height: 16,
                   ),
                   PlaceFeeCard(provider.placeFeeController, (String value) {
@@ -298,42 +156,6 @@ class AdjustmentPage extends ConsumerWidget {
             ),
           ),
         ));
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('精算'),
-        leading: IconButton(
-          icon: Icon(Icons.clear),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black),
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: cellHeight * provider.pointList.length,
-                  child: ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: provider.pointList.length,
-                    itemBuilder: (context, index) {
-                      return scoreRowWidget(index, provider);
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
 
---
+++ lib/src/page/adjustment/viewModel.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../accessor/table/chipScoreProvider.dart';
+import '../../accessor/table/gameSettingProvider.dart';
+import '../../accessor/table/scoreProvider.dart';
+import '../../model/gameSettingModel.dart';
+
+class AdjustmentViewModel extends ChangeNotifier {
+  AdjustmentViewModel(this.ref, this.drId) {
+    listenGameSetting();
+    listenScore();
+    listenChipScore();
+  }
+
+  Ref ref;
+  int drId;
+  List<PointProperty> pointList = [];
+  GameSettingModel gameSetting;
+  List<int> totalList = [];
+  List<ChipScoreModelEx> chipScoreList;
+  TextEditingController placeFeeController = TextEditingController()
+    ..text = '0';
+
+  void listenGameSetting() {
+    final accessor = ref.watch(gameSettingAccessor);
+    if (accessor.isInitialized) {
+      if (accessor.drIdMap.containsKey(drId)) {
+        gameSetting = accessor.drIdMap[drId];
+        setPointPropertyList();
+      }
+    }
+  }
+
+  void listenScore() {
+    final accessor = ref.watch(scoreAccessor);
+    if (accessor.isInitialized) {
+      if (accessor.scoreViewMap.containsKey(drId)) {
+        final drIdScoreView = accessor.scoreViewMap[drId];
+        totalList = [];
+        for (var i = 0; i <= drIdScoreView.maxGameCount; i++) {
+          for (var j = 0; j <= drIdScoreView.maxNumber; j++) {
+            if (drIdScoreView.map[i].containsKey(j) == false) {
+              continue;
+            }
+            if (i == 0) {
+              totalList.add(drIdScoreView.map[i][j].score);
+              continue;
+            }
+            totalList[j] += drIdScoreView.map[i][j].score;
+          }
+        }
+        setPointPropertyList();
+      }
+    }
+  }
+
+  void listenChipScore() {
+    final accessor = ref.watch(chipScoreAccessor);
+    if (accessor.isInitialized) {
+      if (accessor.drIdMap.containsKey(drId)) {
+        chipScoreList = accessor.drIdMap[drId];
+        setPointPropertyList();
+      }
+    }
+  }
+
+  void setPointPropertyList() {
+    if (gameSetting == null) {
+      return;
+    }
+    if (chipScoreList == null) {
+      return;
+    }
+
+    // 場代をセット
+    placeFeeController
+      ..text = gameSetting.placeFee.toString()
+      ..selection = TextSelection.fromPosition(
+        TextPosition(offset: placeFeeController.text.length),
+      );
+
+    // 一人あたりの場代を計算。１の位は余りとしてトップに分配
+    var placeFeePerOne = 0;
+    var reminder = 0;
+    final placeFee = gameSetting.placeFee;
+    if (placeFee != 0) {
+      placeFeePerOne = (placeFee ~/ pointList.length);
+      final onesPlace = int.parse(placeFeePerOne.toString().substring(0, 1));
+      placeFeePerOne -= onesPlace;
+      reminder = (placeFee % pointList.length) + onesPlace * pointList.length;
+    }
+
+    // 表示するポイントを計算する
+    // chipScoreListはgjm分のindexを持っているのでchipScoreをベースにする
+    pointList = [];
+    var topIndex = 0;
+    var topScore = 0;
+    for (final cs in chipScoreList) {
+      final name = cs.name;
+      final chipPoint = cs.score * gameSetting.chipRate;
+      var point = chipPoint;
+      if (totalList.length > cs.number) {
+        final total = totalList[cs.number];
+        if (total > topScore) {
+          topIndex = cs.number;
+          topScore = total;
+        }
+        point += totalList[cs.number] * gameSetting.rate;
+      }
+      pointList.add(PointProperty(name, point, chipPoint, placeFeePerOne));
+    }
+
+    // topにあまりを追加
+    pointList[topIndex].addPlaceFee(reminder);
+
+    notifyListeners();
+  }
+
+  void afterPlaceFeeInput() {
+    final fee = int.parse(placeFeeController.text);
+    if (fee == null) {
+      return;
+    }
+    gameSetting.placeFee = fee;
+    ref.read(gameSettingAccessor).upsert(gameSetting);
+  }
+}
+
+class PointProperty {
+  PointProperty(this.name, this.point, this.chipPoint, this.placeFee) {}
+
+  String name;
+  int point;
+  int chipPoint;
+  int placeFee;
+
+  int get totalPoint {
+    return point + chipPoint - placeFee;
+  }
+
+  void addPlaceFee(int fee) {
+    placeFee += fee;
+  }
+}
